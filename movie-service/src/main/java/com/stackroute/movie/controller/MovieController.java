f22be4f65e434eee08d3b8c71107744c403bcbcd
adityan.a <adityan.a@cgi.com>
2019-10-11 12:47:45 +0530

    Eureka server added

--- movie-service/src/main/java/com/stackroute/movie/controller/MovieController.java
+++ movie-service/src/main/java/com/stackroute/movie/controller/MovieController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "api/v1")
+@RequestMapping(value = "api/v1/movie-service")
 public class MovieController {
     @Autowired
     private MovieService movieService;
--- user-service/src/main/java/com/stackroute/controller/UserController.java
+++ user-service/src/main/java/com/stackroute/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "api/v1/userservice")
+@RequestMapping(value = "api/v1/user-service")
 public class UserController {
 
 
